42ccc6a3fc0c3cbb3f64e0a3e43faf600c6d73a7
Deyb <112176170+dackydeyb@users.noreply.github.com>
2025-02-27 13:45:51 +0800

    save

---
+++ CPE06 - Online Technologies/Online Tech Lab/samplescript.js
@@ -0,0 +1,7 @@
+    document.write("<h1>Hello, world!</h1>")
+
+
+
+    document.write('<a href="https://www.facebook.com">Facebook <br></a>')
+    document.write('<a href="/sakumizuno.jpg">Image<br></a>')
+    document.write('<a href="#">None</a>')
